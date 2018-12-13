fe6d9c8dbf431affa1359414d1ca40352647dff7
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2018-12-13 03:55:03 -0500

    Prueba de Ionic v3

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,7 +6,11 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  notes = [
+    { id:1,title:"Nota 1",description : "Descripcion Nota 1"},
+    { id:2,title:"Nota 2",description : "Descripcion Nota 2"},
+    { id:3,title:"Nota 3",description : "Descripcion Nota 3"},
+  ];
   constructor(public navCtrl: NavController) {
 
   }
